98bd8aad89f87b95b7f91dce9757b56b8991bc3b
Mark Utting <utting@usc.edu.au>
2019-04-13 20:25:21 +1000

    Add Documentation

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -7,17 +7,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.net.URL;
 
+/**
+ * NOTE: Do NOT run this class in IntelliJ.  Run 'RunGui' instead.
+ */
 public class GuiMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("class=" + getClass());
-        URL fxml = getClass().getResource("garden_gui.fxml");
-        System.out.println("URL=" + fxml);
-        Parent root = FXMLLoader.load(fxml);
-        //Button root = new Button("TODO: design garden planner GUI");
+        // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
+        Button root = new Button("TODO: design garden planner GUI");
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
